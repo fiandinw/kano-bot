8b871c413decf6b64da403133efbbccfe9717782
fiandinw <alifiandinursanni@gmail.com>
2021-01-11 14:27:27 +0700

    some test

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 module.exports = async function App(context) {
   if (context.event.isText) {
-    await context.sendText(`received the text message: ${context.event.text}`);
+    //await context.sendText(`${context.event.text}`);
+    if(context.event.text.toLowerCase() === 'kanoping'){//tesbot
+      context.replyText(`pong`);
+    }
+    else if(context.event.text.toLowerCase() === 'kano'){//pangil balik
+      context.getUserProfile().then(profile => {
+      context.replyText(`Hello ${profile.displayName} <3`,{quickReply:{
+        items:[
+          {
+          type: 'action',
+          action: {type: 'message',label: 'ping me',text:'kanoping'}
+          }
+        ]
+      }});
+      });
+    }
   }
 };
